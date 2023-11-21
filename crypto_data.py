45f2437715ea2765930e63c498b78aadddeffba7
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-11-21 18:29:10 +0000

    Define get_coins function

--- crypto_data.py
+++ crypto_data.py
@@ -20,3 +20,33 @@ class Coin:
 
     def __str__(self):
         return f'{self.name} ({self.symbol}): €{self.current_price:,}'
+
+
+def get_coins() -> list[Coin]:
+    '''
+    Get the data from Coingecko
+    '''
+    # Params to be put into request
+    payload: dict = {'vs_currency': 'eur', 'order': 'markect_cap_desc'}
+
+    data = requests.get(BASE_URL, params=payload, timeout=10)
+    json: dict = data.json()
+
+    coin_list: list[Coin] = []
+    for item in json:
+        current_coin: Coin = Coin(name=item.get('name'),
+                                  symbol=item.get('symbol'),
+                                  current_price=item.get('current_price'),
+                                  high_24h=item.get('high_24h'),
+                                  low_24h=item.get('low_24h'),
+                                  price_change_24h=item.get('price_change_24h'),
+                                  price_change_percentage_24h=item.get('price_change_percentage_24h'))
+        coin_list.append(current_coin)
+
+    return coin_list
+
+
+if __name__ == '__main__':
+    coins = get_coins()
+    for coin in coins:
+        print(coin)
